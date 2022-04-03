8c2fd3b39d91d6effc0a10d73c0fb90938d5b3c4
Eric McGuirk <mcguirk.eric@gmail.com>
2022-04-03 16:12:04 -0400

    Add oracle connectivity

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
+import cx_Oracle
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+connection = cx_Oracle.connect(user=os.environ.get("ORACLE_USER"),
+                               password=os.environ.get("ORACLE_PASSWORD"),
+                               dsn=os.environ.get("DATABASE_URL"))
 
 
 @app.route('/')
@@ -15,7 +21,9 @@ def basic():
 
 @app.route('/pro')
 def pro():
-    return render_template("pro_landing.html")
+    cursor = connection.cursor()
+    cursor.execute("SELECT name, AVG(farm_income) income FROM commodity GROUP BY name")
+    return render_template("pro_landing.html", cursor=cursor)
 
 
 if __name__ == '__main__':
